b7fd73f6a1d7acd8ce33d4bb96cf2a51ab59d6de
Shuqian Hon <honsq90@gmail.com>
2018-06-04 23:42:29 +1000

    chore: Use rxjs-marbles instead of jest-marbles

--- src/features/todos/store/effects/todos.effect.spec.ts
+++ src/features/todos/store/effects/todos.effect.spec.ts
@@ -1,21 +1,21 @@
-import { TestBed } from "@angular/core/testing";
-import { provideMockActions } from "@ngrx/effects/testing";
-import { cold, hot } from "jest-marbles";
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { marbles } from 'rxjs-marbles/jest';
 
-import { Observable } from "rxjs";
-import "rxjs/add/observable/of";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
-import { TodosEffects } from "../effects";
-import * as fromActions from "../actions";
-import { TodosService } from "../../services";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
-import { Todo } from "../../models/todo.model";
+import { TodosEffects } from '../effects';
+import * as fromActions from '../actions';
+import { TodosService } from '../../services';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Todo } from '../../models/todo.model';
 
-describe("TodosEffects", () => {
+describe('TodosEffects', () => {
   let effects: TodosEffects;
   let actions: Observable<any>;
   let getTodosMock: Observable<Todo[]>;
-  const t = [{ text: "hi" }];
+  const t = [{ text: 'hi' }];
   const serviceMock = {
     getTodos: jest.fn(),
   };
@@ -33,46 +33,46 @@ describe("TodosEffects", () => {
     effects = TestBed.get(TodosEffects);
   });
 
-  describe("#loadTodos$", () => {
-    it("should not duplicate server queries", () => {
-      const action = new fromActions.LoadTodosAction("hi");
+  describe('#loadTodos$', () => {
+    it('should not duplicate server queries', marbles((m) => {
+      const action = new fromActions.LoadTodosAction('hi');
       const completion = new fromActions.LoadTodosSuccessAction(t);
 
-      actions =         hot("aaaaa", { a: action });
-      getTodosMock =   cold("--t", { t });
-      const expected = cold("--b", { b: completion });
+      actions =         m.hot('aaaaa', { a: action });
+      getTodosMock =   m.cold('--t', { t });
+      const expected = m.cold('--b', { b: completion });
 
       serviceMock.getTodos.mockReturnValue(getTodosMock);
 
-      expect(effects.loadTodos$).toBeObservable(expected);
-    });
+      m.expect(effects.loadTodos$).toBeObservable(expected);
+    }));
 
-    it("should only query again if value has changed", () => {
-      const action = new fromActions.LoadTodosAction("hi");
-      const actionB = new fromActions.LoadTodosAction("hi2");
+    it('should only query again if value has changed', marbles((m) => {
+      const action = new fromActions.LoadTodosAction('hi');
+      const actionB = new fromActions.LoadTodosAction('hi2');
       const completion = new fromActions.LoadTodosSuccessAction(t);
 
-      actions =         hot("aaaaab", { a: action, b: actionB });
-      getTodosMock =   cold("--t", { t });
-      const expected = cold("--b----b", { b: completion });
+      actions =         m.hot('aaaaab', { a: action, b: actionB });
+      getTodosMock =   m.cold('--t', { t });
+      const expected = m.cold('--b----b', { b: completion });
 
       serviceMock.getTodos.mockReturnValue(getTodosMock);
 
-      expect(effects.loadTodos$).toBeObservable(expected);
-    });
+      m.expect(effects.loadTodos$).toBeObservable(expected);
+    }));
 
-    it("should catch error after 3 tries", () => {
-      const error = "failed!";
-      const action = new fromActions.LoadTodosAction("hi");
+    it('should catch error after 3 tries', marbles((m) => {
+      const error = 'failed!';
+      const action = new fromActions.LoadTodosAction('hi');
       const completion = new fromActions.LoadTodosFailAction(error);
 
-      actions =              hot("a", { a: action });
-      const getTodosError = cold("--#", {}, error);
-      const expected =      cold("------e", { e: completion });
+      actions =              m.hot('a', { a: action });
+      const getTodosError = m.cold('--#', {}, error);
+      const expected =      m.cold('------e', { e: completion });
       serviceMock.getTodos.mockReturnValue(getTodosError);
 
-      expect(effects.loadTodos$).toBeObservable(expected);
-    });
-  })
+      m.expect(effects.loadTodos$).toBeObservable(expected);
+    }));
+  });
 
 });
