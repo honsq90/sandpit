7550e039f9496aae1be921e83d2de8e76e76b958
Shuqian Hon <honsq90@gmail.com>
2018-06-16 13:52:25 +1000

    multiplayer canvas via websockets

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,8 +1,17 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
-import { switchMap, takeUntil, pairwise, map, tap } from 'rxjs/operators';
+import { switchMap, takeUntil, pairwise, tap, filter, map } from 'rxjs/operators';
 
-const colors = ['blue', 'red', 'purple', 'gray'];
+const socket = new WebSocket('wss://node2.wsninja.io');
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 @Component({
   selector: 'app-canvas',
@@ -17,12 +26,14 @@ export class CanvasComponent implements AfterViewInit {
   mouseDown$: Observable<MouseEvent>;
   mouseMove$: Observable<MouseEvent>;
   mouseUp$: Observable<MouseEvent>;
+  strokeStyle: string;
 
   ngAfterViewInit() {
     const canvas = this.canvas.nativeElement;
 
     this.setupCanvas(canvas);
     this.handleCanvas(canvas);
+    this.setupWebSockets();
   }
 
   setupCanvas(canvas) {
@@ -42,23 +53,60 @@ export class CanvasComponent implements AfterViewInit {
     this.mouseUp$ = fromEvent(canvas, 'mouseup');
 
     this.mouseDown$.pipe(
-      tap(mouseDown => {
+      tap((mouseDown) => {
         mouseDown.preventDefault();
         mouseDown.stopPropagation();
       }),
-      switchMap(_ => this.mouseMove$
+      switchMap((_) => this.mouseMove$
         .pipe(
           pairwise(),
           takeUntil(this.mouseUp$)
         ))
     ).subscribe(([lastMove, currentMove]) => {
-      this.ctx.strokeStyle = colors[currentMove.offsetX % colors.length];
-      this.ctx.beginPath();
-      this.ctx.moveTo(lastMove.offsetX, lastMove.offsetY);
-      this.ctx.lineTo(currentMove.offsetX, currentMove.offsetY);
-      this.ctx.closePath();
-      this.ctx.stroke();
+      this.drawStroke(this.ctx, [lastMove, currentMove], this.strokeStyle);
+      socket.send(JSON.stringify({
+        type: 'drawStroke',
+        offsetX: currentMove.offsetX,
+        offsetY: currentMove.offsetY,
+        strokeStyle: this.strokeStyle
+      }));
     });
   }
 
+  drawStroke(context, [lastMove, currentMove], strokeStyle) {
+    context.strokeStyle = strokeStyle;
+    context.beginPath();
+    context.moveTo(lastMove.offsetX, lastMove.offsetY);
+    context.lineTo(currentMove.offsetX, currentMove.offsetY);
+    context.closePath();
+    context.stroke();
+  }
+
+  setupWebSockets() {
+    const socketOpen$ = fromEvent(socket, 'open', { once: true });
+    const socketMessageDrawStroke$: Observable<[DrawStrokeSocketEvent, DrawStrokeSocketEvent]> = fromEvent(socket, 'message')
+      .pipe(
+        map((event: MessageEvent) => JSON.parse(event.data)),
+        filter((message: DrawStrokeSocketEvent) => {
+          return message.type === 'drawStroke';
+        }),
+        pairwise(),
+    );
+
+    socketOpen$
+      .subscribe((_) => {
+        this.strokeStyle = getRandomColor();
+        socket.send(JSON.stringify({ guid: '2b39803b-cb62-460b-8a04-c8dfc7f3b37c' }));
+      });
+
+    socketMessageDrawStroke$
+      .subscribe((message) => {
+        this.drawStroke(this.ctx, message, message[0].strokeStyle);
+      });
+  }
+}
+
+class DrawStrokeSocketEvent {
+  type = 'drawStroke';
+  constructor(public offsetX: string, public offsetY: string, public strokeStyle: string) { }
 }
