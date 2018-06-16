df772503d3953a8460da7ffcc8361559c4c8f205
Shuqian Hon <honsq90@gmail.com>
2018-06-16 17:29:42 +1000

    try phoenix channel observables

--- src/app/containers/app.component.ts
+++ src/app/containers/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhoenixSocket } from '../lib/phoenix-rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    const phoenixSocket = new PhoenixSocket('ws://localhost:4000/socket');
+
+    phoenixSocket.channel('rooms:positive').join()
+      .subscribe((_) => console.log('connected to positive room'));
+
+    phoenixSocket.channel('rooms:negative').join()
+      .subscribe((_) => console.log('connected to negative room'));
+
+    phoenixSocket.channel('rooms:positive')
+      .messages('update')
+      .subscribe((message) => {
+        console.log('positive', message);
+      });
+
+    phoenixSocket.channel('rooms:negative')
+      .messages('update')
+      .subscribe((message) => {
+        console.log('negative', message);
+      });
+  }
 }
---
+++ src/app/lib/phoenix-rxjs.ts
@@ -0,0 +1,57 @@
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
+import { Socket, Channel } from 'phoenix';
+
+export class PhoenixChannel {
+  private channel: Channel;
+
+  constructor(socket: Socket, topic: string, options = {}) {
+    this.channel = socket.channel(topic, options);
+  }
+
+  join() {
+    return new Observable((observer) => {
+      this.channel.join()
+        .receive('ok', (resp) => {
+          console.log('connected', resp);
+          observer.next(resp);
+        })
+        .receive('error', (resp) => {
+          console.error(resp);
+          observer.error(resp);
+        });
+    });
+  }
+
+  push(message: string, payload: any, timeout?: number) {
+    this.channel.push(message, payload, timeout);
+  }
+
+  leave(timeout?: number) {
+    this.channel.leave(timeout);
+  }
+
+  messages(message) {
+    return new Observable((observer) => {
+      this.channel.on(message, (resp) => {
+        observer.next(resp);
+      });
+    }).pipe(
+      share(),
+    );
+  }
+}
+
+export class PhoenixSocket {
+  private socket: Socket;
+
+  constructor(socketUrl) {
+    this.socket = new Socket(socketUrl);
+    this.socket.connect();
+  }
+
+  channel(topic, options = {}) {
+    return new PhoenixChannel(this.socket, topic, options);
+  }
+}
+
