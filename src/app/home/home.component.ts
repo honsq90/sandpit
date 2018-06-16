8f51e17637bad36f035a113e4d500bdc35152282
Shuqian Hon <honsq90@gmail.com>
2018-06-16 10:21:56 +1000

    sticky table headers

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  rowsCount = Array(50);
 }
