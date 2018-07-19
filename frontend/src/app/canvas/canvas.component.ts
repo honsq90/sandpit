8e4628cc814ca30499d936c4a609ab1b0c9f33db
Shuqian Hon <honsq90@gmail.com>
2018-07-20 00:26:06 +1000

    Rename to Sandpit and turn off AOT.

--- frontend/src/app/canvas/canvas.component.ts
+++ frontend/src/app/canvas/canvas.component.ts
@@ -4,7 +4,7 @@ import { PhoenixSocket, PhoenixChannel } from '../lib/phoenix-rxjs';
 
 import { switchMap, takeUntil, pairwise, tap, mergeMap, filter, merge, map } from 'rxjs/operators';
 
-const phoenixSocket = new PhoenixSocket(`ws://${window.location.host}/socket`);
+const phoenixSocket = new PhoenixSocket(`wss://7777b1b8.au.ngrok.io/socket`);
 
 const getRandomColor = () => {
   const letters = '0123456789ABCDEF';
