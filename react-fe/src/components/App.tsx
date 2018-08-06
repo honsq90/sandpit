db761185fd89eddace61ecff6990f1fda571906e
Shuqian Hon <honsq90@gmail.com>
2018-08-06 14:25:06 +1000

    Add Bulma

--- react-fe/src/components/App.tsx
+++ react-fe/src/components/App.tsx
@@ -1,44 +1,30 @@
 import * as React from 'react'
+import 'bulma/css/bulma.css'
+import SlideContainer from './SlideContainer'
 
-import Toggle from './Toggle'
-import ToggleButton from './ToggleButton'
+interface AppProps { }
 
-namespace Layers {
-  export const Layer1 = () => <Layer2 />
-
-  const Layer2 = () => (
-    <Toggle.Consumer>
-      {({ on }) => (
-        <>
-          {on ? 'The button is on' : 'The button is off'}
-          <Layer3 />
-        </>
-      )}
-    </Toggle.Consumer>
-  )
-
-  const Layer3 = () => <Layer4 />
-
-  const Layer4 = () => (
-    <Toggle.Consumer>
-      {({ on, toggle }) => <ToggleButton on={on} onClick={toggle} />}
-    </Toggle.Consumer>
-  )
-}
-
-interface AppProps {
-  onToggle?(on: boolean): void
-}
-
-const App = ({
-  onToggle = (on: boolean) => console.log('ToggleStatus:', on),
-}: AppProps) => {
+const App = ({ }: AppProps) => {
   return (
-    <div className="container" data-testid="toggle-container">
-      <Toggle onToggle={onToggle}>
-        <Layers.Layer1 />
-      </Toggle>
-    </div>
+    <main>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+          </a>
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item">Canvas</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="section">
+        <div className="container">
+          <SlideContainer />
+        </div>
+      </section >
+    </main>
   )
 }
 
---
+++ react-fe/src/components/SlideContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+interface SlideContainerProps { }
+
+const SlideContainer = ({ }: SlideContainerProps) => {
+  return (
+    <div>test</div>
+  )
+}
+
+export default SlideContainer
